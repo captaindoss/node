40c6f720332944103d0be50ecf122b617c55e045
captaindoss <142238984+captaindoss@users.noreply.github.com>
2023-08-15 11:58:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 2266
 
 app.get('/', (req, res) => {
-  res.send('Hello Sailor, have a maratime day!')
+  res.send('Hello Sailor, have a maritime day, - you illiterate s.o.b.!')
 })
 
 app.listen(port, () => {
